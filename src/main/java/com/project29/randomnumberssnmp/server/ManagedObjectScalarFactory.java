81234b2df6cb481dc0949f5fc492512fcfb3b442
antoniosilva <antoniosilva9116@gmail.com>
2017-01-30 17:14:17 +0000

    Managed Object Scalar Factory

--- src/main/java/com/project29/randomnumberssnmp/server/ManagedObjectScalarFactory.java
+++ src/main/java/com/project29/randomnumberssnmp/server/ManagedObjectScalarFactory.java
@@ -3,6 +3,7 @@ package com.project29.randomnumberssnmp.server;
 import org.snmp4j.agent.MOAccess;
 import org.snmp4j.agent.mo.MOAccessImpl;
 import org.snmp4j.agent.mo.MOScalar;
+import org.snmp4j.smi.Integer32;
 import org.snmp4j.smi.OID;
 import org.snmp4j.smi.OctetString;
 import org.snmp4j.smi.Variable;
@@ -18,6 +19,10 @@ public class ManagedObjectScalarFactory {
     private static Variable getVariable(Object value) {
         if (value instanceof String) {
             return new OctetString((String) value);
+        }
+        
+        if(value instanceof Integer){
+            return new Integer32((int)value);
         }
         throw new IllegalArgumentException("Unmanaged Type: " + value.getClass());
     }
